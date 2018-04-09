e3e5d55f1187712cfab8168f7f31a6027062f715
as2565 <alexandra.signoriello@yale.edu>
2018-04-09 19:45:51 -0400

    added 3D dist

--- 3D/dist.py
+++ 3D/dist.py
@@ -6,11 +6,10 @@ def get_distance(Ax,Ay,Az,Bx,By,Bz):
 	return sqrt((Ax-Bx)**2 + (Ay-By)**2 + (Az-Bz)**2)
 
 
-
-
+# generate two random points
 Ax,Ay,Az = rand(3)
 Bx,By,Bz = rand(3)
 
+# get distance between points
 d = get_distance(Ax,Ay,Az,Bx,By,Bz)
-
-
+print d
