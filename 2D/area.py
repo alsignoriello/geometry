80a579d6dbf7e99e6d5908b8457f3f87c84e9b95
as2565 <alexandra.signoriello@yale.edu>
2018-04-09 19:34:17 -0400

    added area

---
+++ 2D/area.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+import sys
+import numpy as np 
+
+# compute area for vertices
+# vertices = (N,2) with (x,y)
+def get_area(vertices):
+	N = vertices.shape[0]
+	cross = 0.
+	for i in range(0,N):
+		x1,y1 = vertices[i,:]
+		if i + 1 < N:
+			x2,y2 = vertices[i+1,:]
+		else:
+			x2,y2 = vertices[0,:]
+		cross += ((x1*y2) - (x2*y1))
+	return 0.5*cross
+
+
+
+# vertex file
+file = sys.argv[1]
+
+# read in 2D polygon
+vertices = np.loadtxt(file)
+
+# compute area
+a = get_area(vertices)
+print a
