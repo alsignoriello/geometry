977a7d4868324cfc7696fe078ac0704bc9a6831b
as2565 <alexandra.signoriello@yale.edu>
2018-05-08 10:23:54 -0400

    added centers

--- 3D/center.py
+++ 3D/center.py
@@ -2,27 +2,34 @@
 import sys
 import numpy as np 
 
-# compute center for vertices
-# vertices = (N,2) with (x,y)
+
+# compute center for vertics
+# vertices = (N,2) with (x,y,z)
 def get_center(vertices):
 	N = vertices.shape[0]
-	sumx = 0.
-	sumy = 0.
+	sumx = 0
+	sumy = 0
+	sumz = 0
 	for i in range(0,N):
 		sumx += vertices[i,0]
 		sumy += vertices[i,1]
-	cx = sumx / N
-	cy = sumy / N
+		sumz += vertices[i,2]
+	cx = sumx/N
+	cy = sumy/N
+	cz = sumz/N 
 
-	return cx,cy
+	return cx,cy,cz
 
 
 # vertex file
 file = sys.argv[1]
 
-# read in 2D polygon
+
+# load data
 vertices = np.loadtxt(file)
 
 # compute center of mass
-cx,cy = get_center(vertices)
-print cx,cy
+cx,cy,cz = get_center(vertices)
+print cx,cy,cz
+
+
